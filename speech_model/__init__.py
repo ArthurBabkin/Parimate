283ad4e0b128a4531d1f7e0e901ea61ee66ae456
Arthur Babkin <artur.babkin.2015@gmail.com>
2025-03-14 19:17:59 +0300

    ml/delete-false previous commits

--- speech_model/__init__.py
+++
@@ -1,5 +0,0 @@
-from .model import YandexSpeechKit
-from .audio_processor import AudioProcessor
-from .speech_recognizer import SpeechRecognizer
-
-__all__ = ['YandexSpeechKit', 'AudioProcessor', 'SpeechRecognizer'] 
--- test_temp.py
+++
@@ -1,5 +0,0 @@
-from pipeline import SpeechValidator
-
-sv = SpeechValidator()
-
-sv.validate_pronunciation(audio_file_path, target_word) -> bool
