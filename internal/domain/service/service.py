4bcb5f18c7bf764f3e34dda8063f7e9fe9797dae
Dmitry057 <d.tetkin@innopolis.university>
2025-03-15 01:33:42 +0300

    dev/telegram-bot-update

--- internal/domain/service/service.py
+++ internal/domain/service/service.py
@@ -4,25 +4,44 @@ from omegaconf import DictConfig
 
 from internal.adapter.database.sql import UserAdapter, UserPhotoAdapter
 from internal.domain.deepfake import DeepFake
-
+from internal.domain.face_analysis import FaceAnalysis
 
 class ParimateSerive:
     def __init__(self, cfg: DictConfig, df: DeepFake,
                  user_adapter: UserAdapter,
                  user_photo_adapter: UserPhotoAdapter):
+        self.cfg = cfg
         self.df = df
         self.user_adapter = user_adapter
         self.user_photo_adapter = user_photo_adapter
+        self.tasks = []
 
     def insert_photo(self, user_id: int, embeddings):
         self.user_photo_adapter.insert_photo(user_id, embeddings)
 
     def _verify_video_metadata(self, video_path: str):
         return self.df.check_video(video_path)
-
+    
+    def create_task(self, user_id: int, name: str, description: str, phrase: str):
+        
+        self.tasks.append({
+            "user_id": user_id,
+            "name": name,
+            "description": description,
+            "phrase": phrase
+        })
+        
+        return True
+    
     def done_task(self, user_id: int, video_path: str):
         v = self._verify_video_metadata(video_path)
 
         os.remove(video_path)
 
         return v
+    def get_tasks(self, user_id: int):
+        return self.tasks
+    
+    def get_embedings(self, image_base64: str):
+       faceAnalysis = FaceAnalysis(self.cfg)
+       return faceAnalysis.extract_embedding_b64(image_base64)
--- internal/transport/tg/telegram_bot.py
+++ internal/transport/tg/telegram_bot.py
@@ -4,12 +4,11 @@ import random
 from omegaconf import DictConfig
 from telegram import Update
 from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes,
-                          ConversationHandler, MessageHandler, filters)
+                          ConversationHandler, MessageHandler, filters, CallbackQueryHandler)
 
 from internal.domain.service import ParimateSerive
-
-WAITING_FOR_VIDEO = 1
-
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from deepface import DeepFace
 
 class ParimateBot:
     def __init__(self, cfg: DictConfig, service: ParimateSerive):
@@ -20,111 +19,154 @@ class ParimateBot:
                            context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Hello! I'm Parimate")
         await update.message.reply_text("Now send photo of your face")
+        async def handle_photo_request(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+            if not update.message.photo:
+                await update.message.reply_text("Send me a photo of your face 😀")
+                return
+            user_id = update.message.from_user.id
+            file_id = update.message.photo[-1].file_id
 
-        photo_handler = MessageHandler(filters.ALL, self.handle_photo_request)
+            image_base64 = await context.application.bot.get_file(file_id)
+            embeddings = self.convert_to_embeddings(image_base64)
+            try:
+                self.service.insert_photo(user_id, embeddings)
+            except Exception as e:
+                await update.message.reply_text("Error saving photo: " + str(e))
+                return
+            await update.message.reply_text("Photo saved!")
+            context.application.remove_handler(photo_handler)
+            
+        photo_handler = MessageHandler(filters.ALL, handle_photo_request)
         context.application.add_handler(photo_handler)
-
-    async def handle_message(self, update: Update,
-                             context: ContextTypes.DEFAULT_TYPE):
-        # send the type of last message from user. text, video, photo, etc.
-        message_type = "unknown"
-        if update.message.text:
-            message_type = "text"
-        elif update.message.photo:
-            message_type = "photo"
-        elif update.message.video:
-            message_type = "video"
-        elif update.message.audio:
-            message_type = "audio"
-        elif update.message.document:
-            message_type = "document"
-        elif update.message.voice:
-            message_type = "voice"
-
-        await update.message.reply_text(f"Last message type: {message_type}")
-
-    async def handle_photo_request(self, update, context):
-        if not update.message.photo:
-            await update.message.reply_text("Send me a photo of your face 😀")
-            return
-        user_id = update.message.from_user.id
-        file_id = update.message.photo[-1].file_id
-
-        image_base64 = await context.application.bot.get_file(file_id)
-        embeddings = self.convert_to_embeddings(image_base64)
-        try:
-            self.service.insert_photo(user_id, embeddings)
-        except Exception as e:
-            await update.message.reply_text("Error saving photo: " + str(e))
-            return
-        await update.message.reply_text("Photo saved!")
 
     async def handle_create_task(self, update: Update,
                                  context: ContextTypes.DEFAULT_TYPE):
+        async def handle_task_name(update: Update,
+                                   context: ContextTypes.DEFAULT_TYPE):
+            context.user_data["task_name"] = update.message.text
+            await update.message.reply_text("Enter the task description:")
+            context.application.remove_handler(task_name_handler)
+            context.application.add_handler(task_description_handler)
+
+        async def handle_task_description(update: Update,
+                                          context: ContextTypes.DEFAULT_TYPE):
+            context.user_data["task_description"] = update.message.text
+            await update.message.reply_text("Enter the code phrase:")
+            context.application.remove_handler(task_description_handler)
+            context.application.add_handler(task_code_phrase_handler)
+
+        async def handle_task_code_phrase(update: Update,
+                                          context: ContextTypes.DEFAULT_TYPE):
+            context.user_data["task_code_phrase"] = update.message.text
+            user_id = update.message.from_user.id
+            try:
+                self.service.create_task(
+                    user_id,
+                    context.user_data["task_name"],
+                    context.user_data["task_description"],
+                    context.user_data["task_code_phrase"],
+                )
+            except Exception as e:
+                await update.message.reply_text("Error creating task: " + str(e))
+                return
+            await update.message.reply_text("Task created!")
+            context.application.remove_handler(task_code_phrase_handler)
+
+        task_name_handler = MessageHandler(filters.TEXT, handle_task_name)
+        task_description_handler = MessageHandler(filters.TEXT,
+                                                  handle_task_description)
+        task_code_phrase_handler = MessageHandler(filters.TEXT,
+                                                  handle_task_code_phrase)
+
         await update.message.reply_text("Enter the task name:")
-        task_name = update.message.text
-        await update.message.reply_text(
-            f"Task {task_name} created successfully!")
+        context.application.add_handler(task_name_handler)
 
     async def handle_done_task(self, update: Update,
                                context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text("Пожалуйста, отправьте видео "
-                                        "для обработки.")
-        return WAITING_FOR_VIDEO
+            
+        async def handle_task_selection(update: Update,
+                                        context: ContextTypes.DEFAULT_TYPE):
 
-    async def handle_video(self, update: Update,
-                           context: ContextTypes.DEFAULT_TYPE):
-        if not update.message.video:
-            await update.message.reply_text("Это не видео. "
-                                            "Пожалуйста, отправьте видео.")
-            return WAITING_FOR_VIDEO
+            await update.callback_query.message.delete()
+            task_index = int(update.callback_query.data.split("_")[1])
+            context.user_data["task_index"] = task_index
+            task_name = self.service.get_tasks(update.callback_query.from_user.id)[task_index]["name"]
+            await update.callback_query.message.reply_text(f"Отправь видео, которое подтвердит \"{task_name}\"")
+               
+            context.application.remove_handler(callback_handler)
+            
+            context.application.add_handler(video_handler)
+            
+        async def handle_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+            if not update.message.video:
+                await update.message.reply_text("Это не видео. "
+                                "Пожалуйста, отправьте видео.")
+                return WAITING_FOR_VIDEO
 
+            user_id = update.message.from_user.id
+            file_id = update.message.video.file_id
+
+            try:
+                video_file = await context.bot.get_file(file_id)
+                video_path = await video_file.download_to_drive()
+                print(f"Обрабатываем видео..")
+                result = self.service.done_task(user_id, video_path)
+                
+                if(result == "error"):
+                    result = "Не подтверждено"
+                await update.message.reply_text(
+                    f"Видео успешно обработано! Video result: {result}"
+                )
+            except Exception as e:
+                print(f"Ошибка при обработке видео: {e}")
+                await update.message.reply_text(
+                    "Произошла ошибка при обработке видео. "
+                    "Пожалуйста, попробуйте снова."
+                )
+
+            return ConversationHandler.END
+
+        await update.message.reply_text("Конечно! "
+                                        "Выберите задачу:")
         user_id = update.message.from_user.id
-        file_id = update.message.video.file_id
+        tasks = self.service.get_tasks(user_id)
+        if not tasks:
+            await update.message.reply_text("No tasks found.")
+            return ConversationHandler.END
 
-        try:
-            video_file = await context.bot.get_file(file_id)
-            video_path = await video_file.download_to_drive()
+        keyboard = [
+            [InlineKeyboardButton(task['name'], callback_data=f"task_{i}")]
+            for i, task in enumerate(tasks)
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
 
-            result = self.service.done_task(user_id, video_path)
+        callback_handler = CallbackQueryHandler(handle_task_selection, pattern=r"^task_\d+$")
+        video_handler = MessageHandler(filters.VIDEO, handle_video)
 
-            result = f"Видео успешно обработано! Video result: {result}"
+       
+        context.application.add_handler(callback_handler)
 
-            await update.message.reply_text(result)
-
-        except Exception as e:
-            print(f"Ошибка при обработке видео: {e}")
-            await update.message.reply_text("Произошла ошибка при обработке "
-                                            "видео. Пожалуйста, попробуйте "
-                                            "снова.")
-
-        return ConversationHandler.END  # Завершаем диалог
+        await update.message.reply_text("Select task:", reply_markup=reply_markup)
+        
 
     async def cancel(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Действие отменено.")
         return ConversationHandler.END
 
     def register_handlers(self):
-        conv_handler = ConversationHandler(
-            entry_points=[CommandHandler("done_task", self.handle_done_task)],
-            states={
-                WAITING_FOR_VIDEO: [
-                    MessageHandler(filters.VIDEO, self.handle_video)],
-            },
-            fallbacks=[CommandHandler("cancel", self.cancel)],
-        )
+        
         self.app.add_handler(CommandHandler("start",
                                             self.handle_start))
-        self.app.add_handler(conv_handler)
         self.app.add_handler(CommandHandler("create_task",
                                             self.handle_create_task))
+        self.app.add_handler(CommandHandler("done_task",
+                                            self.handle_done_task))
 
     def run(self):
         self.register_handlers()
         self.app.run_polling(allowed_updates=Update.ALL_TYPES)
 
-    @staticmethod
-    def convert_to_embeddings(image_byte64):
+    def convert_to_embeddings(self,image_byte64):
         # Convert image to embeddings
-        embeddings = [random.random() for _ in range(512)]
-        return ','.join(map(str, embeddings))
+        return self.service.get_embedings(image_byte64)
+        
