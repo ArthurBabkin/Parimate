371b3ea23069c4c78a7ee62e51a73efba2d5e9ea
Ilya Merkulov <merkuloviv@my.msu.ru>
2025-03-14 19:53:25 +0300

    add __init__.py

--- internal/domain/face_analysis/face_analysis.py
+++ internal/domain/face_analysis/face_analysis.py
@@ -17,9 +17,10 @@ class FaceAnalysis:
             image, detector_backend=self.detector, model_name=self.model_name
         )
         if len(embeddings) != 1:
-            raise ValueError("There should be exactly one face in the reference image.")
+            raise ValueError(
+                "There should be exactly one face in the reference image.")
         return embeddings[0]['embedding']
-    
+
     def extract_embedding_b64(self, image: str) -> list[float]:
         """
         Converts a base64-encoded image to a NumPy array and extracts its face embedding.
@@ -29,15 +30,18 @@ class FaceAnalysis:
     def verify_face(self, image: np.ndarray, reference: list[float]) -> bool:
         """Verifies if two face images belong to the same person."""
         result = DeepFace.verify(
-            image, reference, detector_backend=self.detector, model_name=self.model_name, silent=True
+            image, reference, detector_backend=self.detector,
+            model_name=self.model_name, silent=True
         )
         return result.get('verified', False)
-    
+
     def verify_face_b64(self, image: str, reference: list[float]):
         return self.verify_face(convert_base64_to_np(image), reference)
+
 
 def convert_base64_to_np(img_b64: str) -> np.ndarray:
     """
     Converts a base64-encoded image string into a NumPy array.
     """
-    return cv2.imdecode(np.frombuffer(base64.b64decode(img_b64), dtype=np.uint8), -1)
+    return cv2.imdecode(
+        np.frombuffer(base64.b64decode(img_b64), dtype=np.uint8), -1)
