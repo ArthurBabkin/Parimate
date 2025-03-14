a03a53bc322649c8c1116337e43f729fe2f6d3e1
Daria <43551406+sidsnow@users.noreply.github.com>
2025-03-14 14:52:25 +0300

    add verify for b64

--- src/face_analysis.py
+++ src/face_analysis.py
@@ -8,7 +8,7 @@ class FaceAnalysis:
         self.detector = detector
         self.model_name = model_name
 
-    def extract_embedding(self, image: np.ndarray) -> np.ndarray:
+    def extract_embedding(self, image: np.ndarray) -> list[float]:
         """Extracts face embedding from an image."""
         embeddings = DeepFace.represent(
             image, detector_backend=self.detector, model_name=self.model_name
@@ -17,18 +17,21 @@ class FaceAnalysis:
             raise ValueError("There should be exactly one face in the reference image.")
         return embeddings[0]['embedding']
     
-    def extract_embedding_b64(self, image: str) -> np.ndarray:
+    def extract_embedding_b64(self, image: str) -> list[float]:
         """
         Converts a base64-encoded image to a NumPy array and extracts its face embedding.
         """
         return self.extract_embedding(convert_base64_to_np(image))
 
-    def verify_face(self, image: np.ndarray, reference: np.ndarray) -> bool:
+    def verify_face(self, image: np.ndarray, reference: list[float]) -> bool:
         """Verifies if two face images belong to the same person."""
         result = DeepFace.verify(
             image, reference, detector_backend=self.detector, model_name=self.model_name, silent=True
         )
         return result.get('verified', False)
+    
+    def verify_face_b64(self, image: str, reference: list[float]):
+        return self.verify_face(convert_base64_to_np(image), reference)
 
 def convert_base64_to_np(img_b64: str) -> np.ndarray:
     """
